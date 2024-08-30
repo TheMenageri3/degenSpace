d5aad89447484afdf89b508d1c3ffa6de034715e
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-08-30 16:33:37 +0530

    fix : bg

--- src/components/RightSideBar/RightSideBar.tsx
+++ src/components/RightSideBar/RightSideBar.tsx
@@ -3,7 +3,7 @@ import P from "../P";
 export const RightSideBar = () => {
   return (
     <div className="flex flex-col w-1/3 h-full p-2 border-l-[3px] border-border items-center">
-      <div className="mt-[20px]">
+      <div className="mt-[20px] flex flex-col gap-[10px] w-full h-full items-center">
         <P className="font-bold">...</P>
       </div>
     </div>
