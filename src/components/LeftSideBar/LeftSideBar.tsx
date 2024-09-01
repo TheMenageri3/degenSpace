eb674b9ccf1e46e67d5bb8cb08f4d56bada8bf28
Nagaprasadvr <nagaprasadvr246@gmail.com>
2024-09-01 08:00:00 +0530

    feat: beatify

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "../Logo";
 import P from "../P";
 import { Wallet } from "./Wallet";
 
@@ -5,7 +6,7 @@ export const LeftSideBar = () => {
   return (
     <div className="w-1/3 h-full p-2 border-r-[3px] border-border flex flex-col items-center">
       <div className="mt-[20px] flex flex-col gap-[10px] w-full h-full items-center">
-        <P className="text-lg font-bold">degenSpace</P>
+        <Logo />
         <Wallet />
       </div>
     </div>
